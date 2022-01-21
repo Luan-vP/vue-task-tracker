27227308f365cf21fa8af8058083901d39da6ec4
Luan van Pletsen <luanvanpletsen@gmail.com>
2022-01-21 11:51:08 +0000

    Hello World

---
+++ app.js
@@ -0,0 +1,7 @@
+const App = Vue.createApp({
+    template: '<h1>Hello World</h1>'
+});
+
+// mount the app to the div in index.html with the id 'app'
+// elements are created there according to the app requirements
+App.mount('#app');
